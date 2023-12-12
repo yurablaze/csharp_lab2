b954f365585082929249cf4bd3236cf0d7ed4031
yurablaze <123833346+yurablaze@users.noreply.github.com>
2023-12-12 02:33:51 +0200

    Добавьте файлы проекта.

---
+++ Cubek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Cube
+{
+    public double Sidelenght { get; set; }
+
+    public Cube(double sidelenght)
+    {
+        Sidelenght = sidelenght;
+    }
+
+
+    public double Volume()
+    {
+        return Math.Pow(Sidelenght, 3);
+    }
+
+    public double Area()
+    {
+        return 6 * Math.Pow(Sidelenght, 2);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Cube calculate = new Cube(5);
+        Console.WriteLine("Об'єм: " + calculate.Volume());
+        Console.WriteLine("Площа: " + calculate.Area());
+    }
+}
+
